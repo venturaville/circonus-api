d6570da9fec176e375fdedb86be15b104c53b6d3
David Nicklay <david.nicklay@turner.com>
2013-11-01 12:34:56 -0400

    updating all composites

--- bin/update_all_composites.rb
+++ bin/update_all_composites.rb
@@ -9,19 +9,10 @@ require 'optparse'
 require "#{ENV['HOME']}/.circonus.rb"
 
 def do_update_check_bundle(data)
-  search_check_bundle = @cached_list_check_bundle.select { |s| s['display_name'] == data['display_name'] }
-  existing = false
-  if search_check_bundle.any? # already exists...
-    existing = true
-    #pp search_check_bundle.first['_cid']
-    #pp data
-    r = @c.update_check_bundle(search_check_bundle.first['_cid'],data)
-  else
-    r = @c.add_check_bundle(data)
-  end
+  r = @c.update_check_bundle(search_check_bundle.first['_cid'],data)
   if not r.nil? then
     pp r
-    print "Success (#{existing ? 'updating' : 'adding'} #{data['display_name']})\n"
+    print "Success updating #{data['display_name']})\n"
   end
 end
 
@@ -59,11 +50,9 @@ def composite_update(composite,tags,select_tags)
 
   new_formula = generate_formula(checkids,tags['consolidation'],tags['datatype'],tags['metric'])
   if composite['config']['formula'] != new_formula
-    puts "Needs updating!"
-    # --TODO actually do the update
+    composite['config']['formula'] = new_formula
+    do_update_check_bundle(composite)
   end
-    puts "old_formula: #{composite['config']['formula']}"
-    puts "new_formula: #{new_formula}"
 end
 
 automation_tags = %w{ consolidation datatype type source metric }
@@ -77,62 +66,5 @@ composites.each do |composite|
   next if tags['type'].empty? or tags['datatype'].empty? or tags['consolidation'].empty? or tags['metric'].empty?
 
   composite_update(composite,tags,select_tags)
-  #puts cbs.map { |m| m['display_name'] }
-
-  # TODO: test for composite changes and update as needed
-
-end
-
-__END__
-checkbundles = @cached_list_check_bundle.select { |s| ((s['tags'].sort.uniq & options[:tags]) == options[:tags]) and (s['type'] == options[:checkbundletype]) }
-
-# unique metric names:
-metrics = checkbundles.map { |m| m['metrics'].map { |mn| mn['name'] } }.flatten.sort.uniq
-if options[:metric]
-  if not metrics.include? options[:metric]
-    raise "No matching metric name (#{options[:metric]}) found in check bundle"
-  else
-    metrics = [options[:metric]]
-  end
-end
-
-# checkids in the group:
-checkids = checkbundles.map { |m| m['_checks'] }.flatten
-
-puts metrics.inspect
-metrics.each do |metric|
-  formula = '(' + checkids.map { |cid| "metric:#{options[:datatype]}(#{cid.split('/').last}, \"#{metric}\", 60000)" }.join(" + ") + ')'
-  bundle = {
-    "brokers"=>[agentid],
-    "config"=>{
-      "formula"=>formula,
-      "composite_metric_name"=>metric
-    },
-    "display_name"=>"Composite Sum: #{options[:tags].join(',')} - #{metric}",
-    "metrics"=>[
-      {"name"=>metric, "status"=>"active", "type"=>"numeric"}
-    ],
-    "notes"=>nil,
-    "period"=>60,
-    "status"=>"active",
-    "tags"=>(options[:tags] + options[:automation_tags] + ["metric:#{metric}"]),
-    "target"=>"composite",
-    "timeout"=>10,
-    "type"=>"composite"
-  }
-
-  if options[:consolidation] == 'sum'
-    # Create total of metrics
-    do_update_check_bundle(bundle)
-  end
-
-  # Get average of metrics
-  bundle['config']['formula'] = "#{formula} / #{checkids.length}"
-  bundle['config']['composite_metric_name'] = "#{metric}_avg"
-  bundle['display_name']="Composite Avg: #{options[:tags].join(',')} - #{metric}"
-  bundle['metrics'].first['name'] = "#{metric}_avg"
-  if options[:consolidation] == 'average'
-    do_update_check_bundle(bundle)
-  end
 end
 
